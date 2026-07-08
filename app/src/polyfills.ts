cb4f6894bb790ad14747d52e32cde57ce0f4a0f4
odingaval <odingaval71@gmail.com>
2026-07-09 00:33:26 +0300

    fix(app): add Window.EventEmitter type declaration for Vercel build

--- app/src/polyfills.ts
+++ app/src/polyfills.ts
@@ -2,6 +2,12 @@ import { Buffer } from 'buffer';
 import { EventEmitter } from 'events';
 import process from 'process';
 
+declare global {
+  interface Window {
+    EventEmitter: any;
+  }
+}
+
 window.Buffer = Buffer;
 window.global = window;
 window.EventEmitter = EventEmitter as any;
